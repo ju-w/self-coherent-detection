6284b5fc3630a99c50ad4e5a91d2220590897328
Julian Wittmann <julian.wittmann@tu-ilmenau.de>
2020-12-16 19:33:35 +0100

    Styling

--- SS_FDE_A.m
+++ SS_FDE_A.m
@@ -1,5 +1,5 @@
 % Single Sideband
-% Single carrier Frequency domain 
+% Single carrier Frequency domain
 pkg load communications
 
 clear all
@@ -9,9 +9,9 @@ SPEC = 1;
 Nblocks = 2; % round( 1/PbRef / N );
 
 M = 16;
-L=100; % length in km -> kleiner Wert für Back-To-Back
-Rb=40*1.07e9;
-D=17;
+L = 100; % length in km -> kleiner Wert für Back-To-Back
+Rb = 40*1.07e9;
+D = 17;
 
 BW_MUX = 2*43e9;
 
@@ -22,8 +22,8 @@ NdelayRRC=10; % don't change ..
 
 % FFT parameters
 N = 4*256; % block size in symbols incl. CP
-Lcp = 4*32;  % length of CP 
-PbRef=1e-3;
+Lcp = 4*32;  % length of CP
+PbRef = 1e-3;
 
 % channel estimation based on an m-sequence
 N1010 = 200;
@@ -38,65 +38,65 @@ fs = BW_MUX*4; % simulation bandwidth
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Ts = log2(M)/Rb*(N-Lcp)/N; % QAM/ PSK symbol interval
-Tb=1/Rb;
-DeltaF=3/Ts;               % Diff. between center freq. of QAM-signal and carrier
+Tb = 1/Rb;
+DeltaF = 3/Ts;               % Diff. between center freq. of QAM-signal and carrier
 fc_mux = (DeltaF + (1+Rcos)/(2*Ts))/2;  % center freq. MUX
 
 Nover = ceil(Ts*fs); % samples per symbol interval
-if mod(Nover,2);
-  Nover=Nover+1;
+if mod(Nover, 2);
+  Nover = Nover+1;
 end
 t0 = Ts/Nover;
-fs=1/t0; % correction to have integer number of symples per symbol interval
+fs = 1/t0; % correction to have integer number of symples per symbol interval
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % transmitted data symbols with values 0..M-1
-S2B = de2bi(0:M-1, log2(M) ); % mapping table: Bits bn to symbols zk
-zk = randi(M, Nblocks*N, 1)-1; % symbols to be transmitted 
-bk_tx = S2B( zk+1,: ); % bits to be transmitted
+S2B = de2bi(0:M-1, log2(M)); % mapping table: Bits bn to symbols zk
+zk = randi(M, Nblocks*N, 1)-1; % symbols to be transmitted
+bk_tx = S2B(zk+1, :); % bits to be transmitted
 
 
 xk = reshape(qammod(zk, M), N, Nblocks); % each column contains a data block
 
-xk_cp = [xk(end-Lcp+1:end,:) ;xk];   % Tx vector with CP (still: % each column is an OFDM-symbol)
-xk_cp = reshape(xk_cp, Nblocks*(N+Lcp),1); % serial, digital transmit signal (electrical) before pulse shaping
+xk_cp = [xk(end-Lcp+1:end, :); xk];   % Tx vector with CP (still: % each column is an OFDM-symbol)
+xk_cp = reshape(xk_cp, Nblocks*(N+Lcp), 1); % serial, digital transmit signal (electrical) before pulse shaping
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% PULSE SHPING and analog signal gerneration
+% PULSE SHAPING and analog signal generation
 %Tx-Filter %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 tt = -NdelayRRC:1/Nover:NdelayRRC; % NdelayRRC symbols delay
 gtTx = rrc(tt, Rcos, 1);
-xt = conv(gtTx, upsample( [xk_train; xk_cp], Nover ) ); % complex time domain signal before upconversion for single sideband
+xt = conv(gtTx, upsample([xk_train; xk_cp], Nover)); % complex time domain signal before upconversion for single sideband
 
-t=[0:length(xt)-1]'*t0;
+t = [0:length(xt)-1]'*t0;
 xt = xt.*exp(j*2*pi*t*DeltaF); % frequeny shifting to get a signle sideband signal
 
-Bias = Rel_bias * mean( abs(xt).^2 );  % referenz-carrier (LO)
+Bias = Rel_bias * mean(abs(xt).^2);  % referenz-carrier (LO)
 
 % optical signal (perfect modulator)
 xt_opt = xt + sqrt(Bias);
-Popt = mean( abs(xt_opt).^2 );
+Popt = mean(abs(xt_opt).^2);
 Eb = Popt*Tb;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % optical transmission
-[yt_opt1, Ndelay1] = mux2_V2(BW_MUX, fc_mux, 2,  t0, xt_opt); % MUX direkt am Sender
+[yt_opt1, Ndelay1] = mux2_V2(BW_MUX, fc_mux, 2, t0, xt_opt); % MUX direkt am Sender
 
 % Glasfaser, lineares Modell: hier: ohne Daempfung, da nur SNR interessiert
 if L>0
   [yt_opt2, Ndelay2] = smf_linV2(0.0, L, D, 0.0, t0, yt_opt1, 1/Tb);
 else
-  Ndelay2=0;
+  Ndelay2 = 0;
   yt_opt2 = yt_opt1;
 end
 
 % mittlere optische Leistung vor optischem Empfangsfilter (Rx-Eingang)
 
 % after demux
-[yt_opt3, Ndelay3] = mux2_V2(BW_MUX, fc_mux, 2,  t0, yt_opt2);  % Signal nach Demux ohne Rauschen
+[yt_opt3, Ndelay3] = mux2_V2(BW_MUX, fc_mux, 2,  t0, yt_opt2); % Signal nach Demux ohne Rauschen
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -107,31 +107,31 @@ Ndelay = Ndelay1 + Ndelay2 + Ndelay3 + (N1010 - Lcp/2 + 2*NdelayRRC)*Nover;
 
 
 %  here an anlog down-conversion is assumed
-t=[0:length(it_Rx)-1]'*t0;
+t = [0:length(it_Rx)-1]'*t0;
 it_complex = it_Rx.*exp(-j*2*pi*t*DeltaF); % frequeny shifting to get a complex baseband signal
 
 % Rx-filtering
 gtRx = gtTx;
-it_complex = conv( it_complex, gtRx );
+it_complex = conv(it_complex, gtRx);
 
 
-% fractional sampling 
+% fractional sampling
 yk_tmp = it_complex(1+Ndelay+2*(Lcp+N)*Nover:Nover/2:end); % 2*(Lcp+N)*Nover due to Golay
-yk = reshape( yk_tmp(1:(Nblocks)*(N+Lcp)*2), (N+Lcp)*2, Nblocks ); % only data part
-yk = yk(2*Lcp+1:end,:); % remove cyclic prefix interval
+yk = reshape(yk_tmp(1:(Nblocks)*(N+Lcp)*2), (N+Lcp)*2, Nblocks); % only data part
+yk = yk(2*Lcp+1:end, :); % remove cyclic prefix interval
 
 Ymu = fft(yk); % spectrum
 
-preamble =  it_complex(1+Ndelay:Nover/2:Ndelay+2*(Lcp+N)*Nover );
-G_EST  = channel_est2(preamble, N, Lcp, 2);
+preamble = it_complex(1+Ndelay:Nover/2:Ndelay+2*(Lcp+N)*Nover);
+G_EST = channel_est2(preamble, N, Lcp, 2);
 
 
 
 % Emu for fraction sampling (equalizer coefficients)
-idx=[1:N];
-Norm=abs( G_EST(idx) ).^2 + abs( G_EST(idx+N) ).^2 ;
+idx = [1:N];
+Norm = abs( G_EST(idx) ).^2 + abs( G_EST(idx+N) ).^2 ;
 Emu = conj( G_EST(idx) )./ Norm;
-idx=[N+1:2*N];
+idx = [N+1:2*N];
 Emu(idx) = conj( G_EST(idx) ) ./ Norm;
 Emu = 2*Emu;
 
@@ -141,10 +141,10 @@ Emu = 2*Emu;
 %%%%%%%%% end of channel  estimation %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % normiertes Signal nach Equalizer
-Ymu = Ymu .* repmat(Emu, 1, Nblocks) ; % 
-idx=[1:N];
-YmuE = 0.5*(Ymu(idx,:) + Ymu(idx+N,:)); % Downsampling in f
-sv_rx = ifft( YmuE );
+Ymu = Ymu .* repmat(Emu, 1, Nblocks);
+idx = [1:N];
+YmuE = 0.5*(Ymu(idx, :) + Ymu(idx+N, :)); % Downsampling in f
+sv_rx = ifft(YmuE);
 
 
 scatterplot(sv_rx(1:end,1));
@@ -153,11 +153,11 @@ scatterplot(sv_rx(1:end,1));
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % optisches Spektrum
 Nwin = (N+L)*Nover; % bestimmt spektrale Auflösung diese ist:
-w = rectwin( Nwin );
+w = rectwin(Nwin);
 f0 = 1/t0/Nwin;
 if SPEC
   figure(3)
-  [Phi_xx, f] = pwelch( xt_opt  , w, 0, Nwin, 1/t0, 'twosided' );
+  [Phi_xx, f] = pwelch(xt_opt, w, 0, Nwin, 1/t0, 'twosided');
   f=[-Nwin/2+1:Nwin/2]*f0;
   plot(f/1e9, mag2db(fftshift(Phi_xx/Phi_xx(2))), 'linewidth', 2);
   axis([-50 80 -30 70])
--- channel_est2.m
+++ channel_est2.m
@@ -5,15 +5,15 @@ function G_EST = channel_est2(preamble, N, L, Nup)
 % Xmu = Rx-Spektren of length 2*(N+L)*Nup (received preambel incl. CP-intervals
 
 N_CS = N;          % lengths of complementary sequences, needs to be power of 2
-x1=preamble(Nup*L+1:(L+N)*Nup);
-x2=preamble(Nup*(N+2*L)+1:end);
+x1 = preamble(Nup*L+1:(L+N)*Nup);
+x2 = preamble(Nup*(N+2*L)+1:end);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % iterative generation of Golay sequences
-P=[1 1 1 -1].';
-Q=[1 1 -1 1].';
+P = [1 1 1 -1].';
+Q = [1 1 -1 1].';
 
-for k=3:log2(N_CS);
+for k = 3:log2(N_CS);
   P_old = P;
   P = [P_old; Q];
   Q = [P_old; -Q];
@@ -21,14 +21,14 @@ end
 
 % these are the Galoy sequences of length N_CS
 %G_A = (-P+1)/2;
-%G_B = (Q+1)/2; 
+%G_B = (Q+1)/2;
 G_A = P;
 G_B = Q;
 
 X_A = conj( fft( upsample(G_A, Nup) ) );
 X_B = conj( fft( upsample(G_B, Nup) ) );
 
-size(x2)
-size(X_B)
+% size(x2)
+% size(X_B)
 
 G_EST = (fft(x1).*X_A + fft(x2).*X_B)/(2*N);
